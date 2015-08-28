1af8e70823cd09646bf44513c54eb5acd5892f8a
fransafu <fransafu@gmail.com>
2015-08-28 06:05:17 -0300

    mejoras para el cliente y se agrego historial.

--- ChatClient.py
+++ ChatClient.py
@@ -2,18 +2,54 @@
 
 import sys
 import asyncio
+import os
 
 ### Esta funcion es llamada cuando el usuario ingresa un mensaje
 ### recibe el mensaje y un socket conectado al servidor
 ### su trabajo es enviar el mensaje
 def enviar_mensaje(mensaje, servidor):
+    os.system('clear')
+    epic_buho()
+    mostrar_historial()
     servidor.write(mensaje.encode())
 
 ### Esta funcion recibe un mensaje desde el servidor
 ### Es su trabajo decidir que hacer con el
 ### Idealmente lo muestra al usuario de alguna manera
 def recibir_mensaje(mensaje):
+    guardar_historial(mensaje.decode())
     print(mensaje.decode())
+
+def guardar_historial(mensaje):
+    ObjArchivo = open(os.getcwd()+'/historial.txt', mode='a', encoding='utf-8')
+    ObjArchivo.write(mensaje)  # escribe cadena1 añadiendo salto de línea
+    ObjArchivo.close  # cierra archivo
+
+def mostrar_historial():
+    archivo = open(os.getcwd()+'/historial.txt','r')  # abre archivo en modo lectura
+    lista = archivo.readlines()  # lee todas la líneas a una lista
+    for linea in lista[limite_inferior(lista):len(lista)]:
+        print(linea, end='')
+    archivo.close
+
+def limite_inferior(lista):
+    limite_inferior = (len(lista) - 10)
+
+    if (limite_inferior <= 0):
+        limite_inferior = 0
+    elif (limite_inferior > 0):
+        limite_inferior = limite_inferior
+    else:
+        print ("esto no pasa :)")
+    return limite_inferior
+
+def epic_buho():
+    print ("""
+    ,___,
+    [O.o]   Welcome to Búho chat
+    /)__)       Have fun!
+    -”–”- 
+    """)
     
 @asyncio.coroutine
 def tcp_echo_client(loop, servidor, puerto):
@@ -29,12 +65,10 @@ def tcp_echo_client(loop, servidor, puerto):
         recibir_mensaje(data)
         
 if __name__ == '__main__':
-    print ("""
-    ,___,
-    [O.o]   Welcome to Búho chat
-    /)__)       Have fun!
-    -”–”- 
-    """)
+    epic_buho()
+
+    ObjArchivo = open(os.getcwd()+'/historial.txt', mode='a', encoding='utf-8')
+    ObjArchivo.close
 
     servidor = sys.argv[1]
     puerto = sys.argv[2]
