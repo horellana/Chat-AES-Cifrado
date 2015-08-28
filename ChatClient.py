8d5f2613792f47978935821febf5b856844a1107
fransafu <fransafu@gmail.com>
2015-08-28 04:08:27 -0300

    Agregando el detalle mas importante, el Búho.

--- ChatClient.py
+++ ChatClient.py
@@ -6,19 +6,24 @@ import asyncio
 @asyncio.coroutine
 def tcp_echo_client(loop, servidor, puerto):
     ### Nos conectamos al servidor
-    r, w = yield from asyncio.open_connection(servidor, puerto,
-                                              loop=loop)
+    r, w = yield from asyncio.open_connection(servidor, puerto, loop=loop)
 
     ### Escuchamos stdin por mensajes enviados por nosotros
     ### (El cliente)
-    loop.add_reader(sys.stdin,
-                    lambda: w.write(sys.stdin.readline().encode()))
+    loop.add_reader(sys.stdin, lambda: w.write(sys.stdin.readline().encode()))
 
     while True:
         data = yield from r.read(100)
         print(data.decode())
         
 if __name__ == '__main__':
+    print """
+    ,___,
+    [O.o]   Welcome to Búho chat
+    /)__)       Have fun!
+    -”–”- 
+    """
+
     servidor = sys.argv[1]
     puerto = sys.argv[2]
     
