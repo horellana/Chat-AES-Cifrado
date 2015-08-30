ba72686d4a122a9ef67e3418f365ca7afe4d628a
Hector Orellana <pulga.p.29@gmail.com>
2015-08-30 02:17:44 -0300

    las variables globales son malas ...

--- ChatServer.py
+++ ChatServer.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python
 
 import sys
-import random
 import asyncio
 
 from Cifrado import Enigma
-
-clientes = []
-contador_usuarios = 0
-
-key = sys.argv[1]
-enigma = Enigma(key)
 
 
 # Esta clase la saque de
 # https://docs.python.org/dev/library/asyncio-protocol.html#tcp-echo-server-protocol
 # Solo la modifique para que guarde a los clientes en la lista `clientes`
-class EchoServerClientProtocol(asyncio.Protocol):
+class ChatServer(asyncio.Protocol):
+    # Estas variables son compartidas por todas las instancias
+    # de la clase `ChatServer`
+    enigma = None
+    clientes = []
+    contador_usuarios = 0
+
+    def __init__(self, key):
+        ChatServer.enigma = Enigma(key)
+
     # Esta funcion es llamada cada vez que un nuevo cliente se conecta
     # Aqui creamos un diccionario con los datos (nombre y socket)
     # y lo guardamos en la lista de clientes.
     # me imagino que transport es el socket, pero no estoy seguro.
     def connection_made(self, transport):
-        global contador_usuarios
-        cliente = {'nombre': 'user{}'.format(contador_usuarios),
+        cliente = {'nombre': 'user{}'.format(ChatServer.contador_usuarios),
                    'transport': transport}
-        clientes.append(cliente)
-        contador_usuarios = contador_usuarios + 1
+        ChatServer.clientes.append(cliente)
+        ChatServer.contador_usuarios += 1
         self.cliente = cliente
-        self.propagar('{} se a conectado'.format(cliente['nombre']))
-    
+        self.propagar('{} se conecto\n'.format(cliente['nombre']))
+
     def propagar(self, mensaje):
-        mensaje = enigma.cifrar(mensaje)
-        for cliente in clientes:
+        mensaje = self.enigma.cifrar(mensaje)
+        for cliente in ChatServer.clientes:
             cliente['transport'].write(mensaje)
 
     # Esta funcion es llamada cuando el cliente envia un mensaje al servidor
     def data_received(self, data):
-        data = enigma.decifrar(data).decode()
         # Aqui enviamos el mensaje a todos los clientes conectados
-        self.propagar('{}: {}'.format(self.cliente['nombre'], data))
+        self.propagar('{}: {}'.format(self.cliente['nombre'],
+                                      self.enigma.decifrar(data)))
 
     def connection_lost(self, exc):
-        global clientes
-        msj = enigma.cifrar('{} Se desconecto'.format(self.cliente['nombre']))
         # Saca al cliente desconectado de la lista ...
-        clientes = [c for c in clientes
-                    if c['nombre'] != self.cliente['nombre']]
-        self.propagar(msj)
+        ChatServer.clientes = [c for c in ChatServer.clientes
+                               if c['nombre'] != self.cliente['nombre']]
+        self.propagar('{} se desconecto\n'.format(self.cliente['nombre']))
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    key = sys.argv[1]
 
-    coro = loop.create_server(EchoServerClientProtocol, '', 8888)
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(lambda: ChatServer(key), '', 8888)
     server = loop.run_until_complete(coro)
 
     print('Serving on {}'.format(server.sockets[0].getsockname()))
