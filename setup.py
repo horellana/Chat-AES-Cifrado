08ca2d928d98c28c0329d786ef1df7525d83a70b
Hector Orellana <pulga.p.29@gmail.com>
2015-09-02 01:08:50 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='Chat-AES-Cifrado',
+      url='https://github.com/juiko/Chat-AES-Cifrado',
+      scripts=['ChatClient.py',
+               'ChatServer.py'])
